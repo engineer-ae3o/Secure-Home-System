71c257c8b193982dbc3fc610b1787eddd7819180
Akemu Elisha Enorhor <akemuelisha07@gmail.com>
2026-05-15 18:45:25 +0100

    Set all pin definitions, as well as resumed keypad scanning logic

--- components/drivers/inc/config.hpp
+++ components/drivers/inc/config.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 
+#include "stm32f103xb.h"
 #include <cstdint>
 
 
@@ -19,6 +20,52 @@ namespace config {
     // Core clock speed being used
     constexpr inline uint32_t CLOCK_SPEED_HZ = 72'000'000UL;
 
-    consteval inline auto bytes_to_words(auto bytes) { return bytes / 4; }
+    struct gpio_pin_t {
+        GPIO_TypeDef* port{};
+        uint32_t pin;
+    };
+
+    // ADC pins to be used for entropy gathering for random number generation
+    constexpr inline gpio_pin_t ADC_PINS[] = {
+        { .port = GPIOA, .pin = GPIO_PIN_0 }, // ADC External Channel 0
+        { .port = GPIOA, .pin = GPIO_PIN_1 }, // ADC External Channel 1
+        { .port = GPIOA, .pin = GPIO_PIN_2 }, // ADC External Channel 2
+        { .port = GPIOA, .pin = GPIO_PIN_3 }, // ADC External Channel 3
+        { .port = GPIOA, .pin = GPIO_PIN_4 }, // ADC External Channel 4
+        { .port = GPIOA, .pin = GPIO_PIN_5 }, // ADC External Channel 5
+        { .port = GPIOA, .pin = GPIO_PIN_6 }, // ADC External Channel 6
+        { .port = GPIOA, .pin = GPIO_PIN_7 }, // ADC External Channel 7
+        { .port = GPIOB, .pin = GPIO_PIN_0 }, // ADC External Channel 8
+        { .port = GPIOB, .pin = GPIO_PIN_1 }  // ADC External Channel 9
+    };
+
+    // LCD I2C pins
+    constexpr inline auto LCD_I2C_PORT = I2C1;
+    constexpr inline gpio_pin_t LCD_SCL = { .port = GPIOB, .pin = GPIO_PIN_6 };
+    constexpr inline gpio_pin_t LCD_SDA = { .port = GPIOB, .pin = GPIO_PIN_7 };
+
+    // GSM module UART pins
+    constexpr inline auto GSM_UART_PORT = USART1;
+    constexpr inline gpio_pin_t GSM_TX = { .port = GPIOA, .pin = GPIO_PIN_9 };
+    constexpr inline gpio_pin_t GSM_RX = { .port = GPIOA, .pin = GPIO_PIN_10 };
+
+    // Keypad matrix pins
+    constexpr inline gpio_pin_t KEYPAD_PINS[] = {
+        { .port = GPIOA, .pin = GPIO_PIN_8 },  // Row 0
+        { .port = GPIOC, .pin = GPIO_PIN_13 }, // Row 1
+        { .port = GPIOC, .pin = GPIO_PIN_14 }, // Row 2
+        { .port = GPIOC, .pin = GPIO_PIN_15 }, // Row 3
+        { .port = GPIOB, .pin = GPIO_PIN_3 },  // Column 0
+        { .port = GPIOB, .pin = GPIO_PIN_4 },  // Column 1
+        { .port = GPIOB, .pin = GPIO_PIN_5 },  // Column 2
+        { .port = GPIOB, .pin = GPIO_PIN_8 },  // Column 3
+    };
+
+    // Reed and Tamper switches' pins
+    constexpr inline gpio_pin_t REED_SWITCH   = { .port = GPIOB, .pin = GPIO_PIN_10 };
+    constexpr inline gpio_pin_t TAMPER_SWITCH = { .port = GPIOB, .pin = GPIO_PIN_11 };
+
+    // Needed for conversion since FreeRTOS uses words
+    consteval inline size_t bytes_to_words(size_t bytes) { return bytes / 4; }
 
 } // namespace config
--- components/drivers/inc/keypad.hpp
+++ components/drivers/inc/keypad.hpp
@@ -17,29 +17,10 @@
 namespace pad {
     
     struct config_t {
-        uint32_t row_1{}, row_2{}, row_3{}, row_4{};
-        uint32_t col_1{}, col_2{}, col_3{}, col_4{};
+        uint32_t row_a_pin{}, row_b_pin{}, row_c_pin{}, row_d_pin{};
+        uint32_t col_a_pin{}, col_b_pin{}, col_c_pin{}, col_d_pin{};
     };
-
-    enum class keys_t : unsigned char {
-        KEY_A     = 'A',
-        KEY_B     = 'B',
-        KEY_C     = 'C',
-        KEY_D     = 'D',
-        KEY_1     = '1',
-        KEY_2     = '2',
-        KEY_3     = '3',
-        KEY_4     = '4',
-        KEY_5     = '5',
-        KEY_6     = '6',
-        KEY_7     = '7',
-        KEY_8     = '8',
-        KEY_9     = '9',
-        KEY_0     = '0',
-        KEY_SHARP = '#',
-        KEY_STAR  = '*'
-    };
-
+    
     template <GPIO_TypeDef* row_port, GPIO_TypeDef* col_port, uint8_t queue_length>
     class key_pad_t {
         private:
@@ -47,15 +28,23 @@ namespace pad {
             config_t m_config{};
 
             QueueHandle_t m_event_queue{};
-            etl::array<uint8_t, (queue_length * sizeof(keys_t))> m_queue_buffer{};
+            etl::array<uint8_t, (queue_length * sizeof(KEYS[0][0]))> m_queue_buffer{};
             StaticQueue_t m_queue_structure{};
 
             TimerHandle_t m_debounce_timer{};
             StaticTimer_t m_debounce_timer_structure{};
-
-        public:
+            
             static constexpr uint8_t DEBOUNCE_TIME_MS = 50;
 
+            // Indexed as `KEYS[row][column]`
+            static constexpr unsigned char KEYS[4][4] = {
+                { '1', '2', '3', 'A' },
+                { '4', '5', '6', 'B' },
+                { '7', '8', '9', 'C' },
+                { '*', '0', '#', 'D' },
+            };
+
+        public:
             key_pad_t(const config_t& config) : m_config(config) {}
 
             ~key_pad_t() noexcept {
@@ -70,11 +59,9 @@ namespace pad {
 
             /**
              * @brief Initializes and configures the gpio pins according to how
-             *        they will be used for row scanning.
+             *        they will be used for the keypad scanning.
              * 
-             * @note User still has to enable and set the priority of the interrupt
-             *       handler being used with NVIC, as well as clear the interrupt pending
-             *       flag in `EXTI->PR`
+             * @note The interrupts still have to be enabled with NVIC
              */
             void init() {
                 ASSERT(!m_is_initialized);
@@ -84,42 +71,40 @@ namespace pad {
                 else if constexpr (col_port == GPIOB) __HAL_RCC_GPIOB_CLK_ENABLE();
                 else if constexpr (col_port == GPIOC) __HAL_RCC_GPIOC_CLK_ENABLE();
                 else if constexpr (col_port == GPIOD) __HAL_RCC_GPIOD_CLK_ENABLE();
-                else static_assert(0);
+                else static_assert(false);
 
                 if constexpr      (row_port == GPIOA) __HAL_RCC_GPIOA_CLK_ENABLE();
                 else if constexpr (row_port == GPIOB) __HAL_RCC_GPIOB_CLK_ENABLE();
                 else if constexpr (row_port == GPIOC) __HAL_RCC_GPIOC_CLK_ENABLE();
                 else if constexpr (row_port == GPIOD) __HAL_RCC_GPIOD_CLK_ENABLE();
-                else static_assert(0);
-
-                // Set all columns as input pullups with interrupts
+                else static_assert(false);
+                
+                // Set all columns as input pullups with interrupt on falling edge
                 GPIO_InitTypeDef col_init = {
-                    .Pin = (m_config.col_a | m_config.col_b | m_config.col_c | m_config.col_d),
-                    .Mode = GPIO_MODE_INPUT,
+                    .Pin = (m_config.col_a_pin | m_config.col_b_pin | m_config.col_c_pin | m_config.col_d_pin),
+                    .Mode = GPIO_MODE_IT_FALLING,
                     .Pull = GPIO_PULLUP,
                     .Speed = GPIO_SPEED_FREQ_LOW
                 };
                 HAL_GPIO_Init(col_port, &col_init);
-
-                EXTI
-
+                
                 // Set all rows as output push pull
                 GPIO_InitTypeDef row_init = {
-                    .Pin = (m_config.row_a | m_config.row_b | m_config.row_c | m_config.row_d),
+                    .Pin = (m_config.row_a_pin | m_config.row_b_pin | m_config.row_c_pin | m_config.row_d_pin),
                     .Mode = GPIO_MODE_OUTPUT_PP,
                     .Pull = GPIO_NOPULL,
                     .Speed = GPIO_SPEED_FREQ_LOW
                 };
                 HAL_GPIO_Init(row_port, &row_init);
 
-                // Set all pins high by default
-                HAL_GPIO_WritePin(row_port, m_config.row_a, GPIO_PIN_SET);
-                HAL_GPIO_WritePin(row_port, m_config.row_b, GPIO_PIN_SET);
-                HAL_GPIO_WritePin(row_port, m_config.row_c, GPIO_PIN_SET);
-                HAL_GPIO_WritePin(row_port, m_config.row_d, GPIO_PIN_SET);
+                // Set all row pins high by default
+                HAL_GPIO_WritePin(row_port, m_config.row_a_pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(row_port, m_config.row_b_pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(row_port, m_config.row_c_pin, GPIO_PIN_SET);
+                HAL_GPIO_WritePin(row_port, m_config.row_d_pin, GPIO_PIN_SET);
 
                 // Can't fail since stack allocated
-                m_event_queue = xQueueCreateStatic(queue_length, sizeof(keys_t), m_queue_buffer.data(), m_queue_structure);
+                m_event_queue = xQueueCreateStatic(queue_length, sizeof(KEYS[0][0]), m_queue_buffer.data(), m_queue_structure);
                 m_debounce_timer = xTimerCreateStatic("Debounce_timer", pdMS_TO_TICKS(DEBOUNCE_TIME_MS), pdFALSE,
                                                        this, debounce_timer_cb, &m_debounce_timer_structure);
 
@@ -127,15 +112,15 @@ namespace pad {
             }
 
             /**
-             * @brief Deinitializes the gpio pins used and sets the pins to analog
-             *        mode so as to reduce power consumption
+             * @brief Deinitializes the gpio pins used and sets the pins to
+             *        analog mode so as to reduce power consumption
              */
             void deinit() {
                 ASSERT(m_is_initialized);
 
                 // Sets all pins as analog to reduce power draw
                 GPIO_InitTypeDef col_init = {
-                    .Pin = (m_config.col_a | m_config.col_b | m_config.col_c | m_config.col_d),
+                    .Pin = (m_config.col_a_pin | m_config.col_b_pin | m_config.col_c_pin | m_config.col_d_pin),
                     .Mode = GPIO_MODE_ANALOG,
                     .Pull = GPIO_NOPULL,
                     .Speed = GPIO_SPEED_FREQ_LOW
@@ -143,7 +128,7 @@ namespace pad {
                 HAL_GPIO_Init(col_port, &col_init);
                 
                 GPIO_InitTypeDef row_init = {
-                    .Pin = (m_config.row_a | m_config.row_b | m_config.row_c | m_config.row_d),
+                    .Pin = (m_config.row_a_pin | m_config.row_b_pin | m_config.row_c_pin | m_config.row_d_pin),
                     .Mode = GPIO_MODE_ANALOG,
                     .Pull = GPIO_NOPULL,
                     .Speed = GPIO_SPEED_FREQ_LOW
@@ -161,6 +146,7 @@ namespace pad {
                 }
 
                 if (m_debounce_timer) {
+                    xTimerStop(m_debounce_timer, portMAX_DELAY);
                     m_debounce_timer_structure = {};
                     m_debounce_timer = nullptr;
                 }
@@ -169,30 +155,52 @@ namespace pad {
             }
 
             /**
+             * @brief Returns the queue in which keypad events are
+             *        pushed into. Pretty straightforward.
              * 
+             * @return The event queue
              */
             QueueHandle_t get_event_queue() const {
+                ASSERT(m_is_initialized);
                 return m_event_queue;
             };
 
             /**
-             * 
+             * @brief This function should be called from all the NVIC
+             *        interrupt handlers for the used gpio pins
              */
             void irq_handler() {
                 // Clear the interrupt pending flags on all pins
-                __HAL_GPIO_EXTI_CLEAR_FLAG(m_config.col_a | m_config.col_b | m_config.col_c | m_config.col_d);
+                __HAL_GPIO_EXTI_CLEAR_IT(m_config.col_a_pin | m_config.col_b_pin | m_config.col_c_pin | m_config.col_d_pin);
                 
-                // And disable interrupts globally on the port
-                // to be reenabled by the debounce timer
-                __HAL_GPIO_EXTI_CLEAR_IT(m_config.col_a | m_config.col_b | m_config.col_c | m_config.col_d);
+                // And disable interrupts globally on the port by masking it.
+                // Will be reenabled by the debounce timer after it's done scanning
+                EXTI->IMR &= ~(m_config.col_a_pin | m_config.col_b_pin | m_config.col_c_pin | m_config.col_d_pin);
 
                 // Start the debounce timer
                 BaseType_t higher_priority_task_woken = pdFALSE;
                 xTimerStartFromISR(m_debounce_timer, &higher_priority_task_woken);
+                portYIELD_FROM_ISR(higher_priority_task_woken);
             }
 
+        private:
             static void debounce_timer_cb(TimerHandle_t xTimer) {
+                // Get timer ID
+                const key_pad_t* keypad = static_cast<key_pad_t*>(pvTimerGetTimerID(xTimer));
+                
+                // Keypad scanning
+                uint8_t row{}, column{};
+                
+                // Send only first detected keypad press to queue
+                xQueueSend(keypad->m_event_queue, &KEYS[row][column], portMAX_DELAY);
 
+                // Clear the interrupt pending flags on all pins before unmasking the EXTI interrupts
+                __HAL_GPIO_EXTI_CLEAR_IT(keypad->m_config.col_a_pin | keypad->m_config.col_b_pin |
+                                         keypad->m_config.col_c_pin | keypad->m_config.col_d_pin);
+                
+                // Enable interrupts by unmasking EXTI interrupts
+                EXTI->IMR |= (keypad->m_config.col_a_pin | keypad->m_config.col_b_pin |
+                              keypad->m_config.col_c_pin | keypad->m_config.col_d_pin);
             }
     };
     
